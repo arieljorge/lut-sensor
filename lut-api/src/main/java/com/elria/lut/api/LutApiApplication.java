b2b90258a4370aac607789cbc4d6ec84860d9c8d
ariel jorge <accarieljorge@gmail.com>
2025-03-19 09:28:44 -0300

    iniciando projeto modulo programado java

---
+++ lut-api/src/main/java/com/elria/lut/api/LutApiApplication.java
@@ -0,0 +1,13 @@
+package com.elria.lut.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LutApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LutApiApplication.class, args);
+    }
+
+}
